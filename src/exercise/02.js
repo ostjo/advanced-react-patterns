af4471e2c4be9ad474910bd2efa1ebd7b37ba9ff
Johanna Osterrieter <90853834+ostjo@users.noreply.github.com>
2024-01-15 10:00:19 +0100

    [2] Compound Components (#2)
    * add exercise 1
    
    * add extra credit 01
    
    * add notes

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -4,29 +4,26 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
-function Toggle() {
+function Toggle({children}) {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
-  // 🐨 replace this with a call to React.Children.map and map each child in
-  // props.children to a clone of that child with the props they need using
-  // React.cloneElement.
-  // 💰 React.Children.map(props.children, child => {/* return child clone here */})
-  // 📜 https://react.dev/reference/react/Children
-  // 📜 https://react.dev/reference/react/cloneElement
-  return <Switch on={on} onClick={toggle} />
+  return React.Children.map(children, child => {
+    // if child is DOM Element, skip props and return as is
+    // otherwise clone and pass props
+    return typeof child.type === 'string'
+      ? child
+      : React.cloneElement(child, {on, toggle})
+  })
 }
 
-// 🐨 Flesh out each of these components
+const ToggleOn = ({on, children}) => (on ? children : null)
 
-// Accepts `on` and `children` props and returns `children` if `on` is true
-const ToggleOn = () => null
+const ToggleOff = ({on, children}) => (on ? null : children)
 
-// Accepts `on` and `children` props and returns `children` if `on` is false
-const ToggleOff = () => null
-
-// Accepts `on` and `toggle` props and returns the <Switch /> with those props.
-const ToggleButton = () => null
+const ToggleButton = ({on, toggle, ...props}) => (
+  <Switch on={on} onClick={toggle} {...props} />
+)
 
 function App() {
   return (
@@ -34,6 +31,7 @@ function App() {
       <Toggle>
         <ToggleOn>The button is on</ToggleOn>
         <ToggleOff>The button is off</ToggleOff>
+        <span>Hello</span>
         <ToggleButton />
       </Toggle>
     </div>
